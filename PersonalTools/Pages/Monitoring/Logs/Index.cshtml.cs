c2283cb95f8b7ea71e295285c58df7d5b56e75c3
jakeh <jakeh@personaltools>
2026-08-21 21:18:48 +0100

    Logs

---
+++ PersonalTools/Pages/Monitoring/Logs/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PersonalTools.Pages.Monitoring.Logs;
+
+public sealed class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
